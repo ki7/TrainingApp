8990dee9fef6bd5c9d628c230164adc6e8948a61
ki7 <assym.el.mahdy@gmail.com>
2026-05-12 21:05:47 +0200

    feat: new icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"], display: "swap" });
 
 export const metadata: Metadata = {
-  title: "Turso Per User Starter",
-  description: "Database per user starter with Turso, Clerk, and SQLite",
+  title: "Assoumax app",
+  description: "Assoumax dojo project",
 };
 
 export default function RootLayout({

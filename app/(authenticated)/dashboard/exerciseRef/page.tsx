dc9c6a960a4efc857892b57e935a527e86c24848
ki7 <assym.el.mahdy@gmail.com>
2026-05-13 11:03:42 +0200

    chore:ui button try out

--- app/(authenticated)/dashboard/exerciseRef/page.tsx
+++ app/(authenticated)/dashboard/exerciseRef/page.tsx
@@ -1,9 +1,15 @@
 import ExerciseDB from "@/app/(authenticated)/dashboard/exerciseRef/ExerciseDB";
+import { Button } from "@/app/components/Button";
 
 export default function ExerciseReferencePage() {
   return (
     <main className="p-6 max-w-xl mx-auto bg-slate-100 rounded">
       <h1 className="text-2xl font-bold mb-6">Working Sets</h1>
+      <Button txt="yep"/>
+      <Button primary txt="nope"/>
+      <Button secondary txt="maybe"/> 
+        
+      
       <h1 className="text-2xl font-bold mb-6">Créer un exercice</h1>
       <ExerciseDB />
     </main>
